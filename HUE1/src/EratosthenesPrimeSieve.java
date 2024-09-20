bbe31c759ec80f52c3a428d465af90325914b2a4
LindnerF22074 <LindnerF22074@sus.htl-grieskirchen.at>
2024-09-20 14:37:19 +0200

    added isPrime method

--- HUE1/src/EratosthenesPrimeSieve.java
+++ HUE1/src/EratosthenesPrimeSieve.java
@@ -7,12 +7,25 @@ public class EratosthenesPrimeSieve implements PrimeSieve{
     @Override
     public boolean isPrime(int p) {
 
+        boolean[] sieve =new boolean[p+1];
+        for (int i=2;i<=p;i++){
+            sieve[i]=false;
+        }
+
+        for (int i=2;i<=Math.sqrt(p);i++){
+            if (!sieve[i]){
+                for (int j=i*i;j<=p;j=j+i){
+                    sieve[j]=true;
+                }
+            }
+        }
+        return sieve[p];
     }
 
     @Override
     public void printPrimes() {
         for (int i=2;i<=max;i++){
-            if(isPrime(i)){
+            if(!isPrime(i)){
                 System.out.println(i);
             }
         }
